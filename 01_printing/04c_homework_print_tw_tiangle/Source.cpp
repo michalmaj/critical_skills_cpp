211fb900e63f18f4a3670a240e7289a1e525309d
Michał Maj <michalpmaj@gmail.com>
2022-12-11 19:04:14 +0100

    Homework

---
+++ 01_printing/04c_homework_print_tw_tiangle/Source.cpp
@@ -0,0 +1,29 @@
+/*
+ * Print using std::cout
+ *	        *
+ *	      * * *
+ *	    * * * * *
+ *	  * * * * * * *
+ *	* * * * * * * * *
+ *	  * * * * * * *
+ *	    * * * * *
+ *		  * * *
+ *		    *
+ */
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "        *" << std::endl;
+	std::cout << "      * * *" << std::endl;
+	std::cout << "    * * * * *" << std::endl;
+	std::cout << "  * * * * * * *" << std::endl;
+	std::cout << "* * * * * * * * *" << std::endl;
+	std::cout << "  * * * * * * *" << std::endl;
+	std::cout << "    * * * * *" << std::endl;
+	std::cout << "      * * *" << std::endl;
+	std::cout << "        *" << std::endl;
+
+	return 0;
+}
