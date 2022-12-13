1814b4fdd8f730faa4a6955f3174dae9c6c1830d
Michał Maj <michalpmaj@gmail.com>
2022-12-13 18:32:43 +0100

    Math Operations

---
+++ 02_data_types_and_variables/04a_math_operations/Source.cpp
@@ -0,0 +1,25 @@
+/*
+ * Write a program that reads 2 number and print their + - * /
+ * Test our code using
+ *	 - zero as first or second number;
+ *	 - negative values;
+ *	 - even and odd value;
+ *	 - INT_MAX;
+ */
+
+#include <iostream>
+
+int main()
+{
+	int a{}, b{};
+	std::cout << "Enter two values: ";
+	std::cin >> a >> b;
+
+	// Do calculations:
+	std::cout << "a + b = " << a + b << std::endl;
+	std::cout << "a - b = " << a - b << std::endl;
+	std::cout << "a * b = " << a * b << std::endl; // If 0 as second number we will get unhandled exception: integer division by zero.
+	std::cout << "a / b = " << a / b << std::endl;
+
+	return 0;
+}
