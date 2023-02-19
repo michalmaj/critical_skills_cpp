1453ad4f788beeb536ba6817dc48e6ae834f14b3
Michał Maj <michalpmaj@gmail.com>
2023-02-19 20:03:54 +0100

    Sum from 1 to N

--- 02_data_types_and_variables/06c_sum_numbers_from_1_to_n/Source.cpp
+++ 02_data_types_and_variables/06c_sum_numbers_from_1_to_n/Source.cpp
@@ -19,8 +19,8 @@ int main()
 	 * 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
 	 * Let's arrange as following
 	 * 1 8		2 7		3 6		4 5		[first number and last number] [second number and second from back]
-	 * What is value of each pair? 9 = N + 1, where N in our case is 8
-	 * How many pairs do we have? 4 = N / 2, where N in our case is 8
+	 * What is value of each pair? 9 = N + 1, where N in our case is 8.
+	 * How many pairs do we have? 4 = N / 2, where N in our case is 8.
 	 *
 	 * So N/2 pair, each has value N+1
 	 * So total sum is (N * (N + 1)) / 2
