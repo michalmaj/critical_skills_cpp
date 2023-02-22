2792464d5f7bbed68988b03a5c42a13ed1ef5b48
Michał Maj <michalpmaj@gmail.com>
2023-02-22 20:18:59 +0100

    our remainder

--- 03_operators/03c_our_remainder/Source.cpp
+++ 03_operators/03c_our_remainder/Source.cpp
@@ -2,6 +2,10 @@
 
 int main()
 {
+	// Let's try 27/12
+	// 27 / 12 = 2 [2 complete units, each is 12]
+	// 2 * 12 = 24 [total complete units]
+	// Remainder is 27 - 24 = 3. This number generate the fractional part.
 	int a{ 27 }, b{ 12 };
 
 	int division = a / b;
