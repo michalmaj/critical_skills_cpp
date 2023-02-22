591e399059a156fc0d1ab16935f872cf2e3f4f98
Michał Maj <michalpmaj@gmail.com>
2023-02-22 20:16:27 +0100

    fractional part

--- 03_operators/03b_fractional_part/Source.cpp
+++ 03_operators/03b_fractional_part/Source.cpp
@@ -9,5 +9,9 @@ int main()
 	double division_part = double_division - int_division;
 	std::cout << "Fraction part: " << division_part << std::endl;
 
+	// Another version
+	double result = a / (b * 1.0);
+	std::cout << "Fraction part: " << result - static_cast<int>(result) << std::endl;
+
 	return 0;
 }
