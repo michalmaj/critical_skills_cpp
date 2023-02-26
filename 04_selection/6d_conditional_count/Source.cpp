05e951f713fbaee34e3f088276c781bf6326fffd
Michał Maj <michalpmaj@gmail.com>
2023-02-26 21:02:53 +0100

    Conditional count

--- 04_selection/6d_conditional_count/Source.cpp
+++ 04_selection/6d_conditional_count/Source.cpp
@@ -3,42 +3,18 @@
 int main()
 {
 	int x, n1, n2, n3, n4, n5;
-	int less_than_X{ 0 };
-	int greater_than_X{ 0 };
+	int cnt{ 0 };
 	std::cout << "Read number X, then other 5 numbers: ";
-	std::cin >> x;
+	std::cin >> x >> n1 >> n2 >> n3 >> n4 >> n5;
 
-	std::cin >> n1;
-	if (n1 <= x)
-		less_than_X++;
-	else
-		greater_than_X++;
+	cnt += (n1 <= x);
+	cnt += (n2 <= x);
+	cnt += (n3 <= x);
+	cnt += (n4 <= x);
+	cnt += (n5 <= x);
 
-	std::cin >> n2;
-	if (n2 <= x)
-		less_than_X++;
-	else
-		greater_than_X++;
+	std::cout << cnt << " " << 5 - cnt;
 
-	std::cin >> n3;
-	if (n3 <= x)
-		less_than_X++;
-	else
-		greater_than_X++;
-
-	std::cin >> n4;
-	if (n4 <= x)
-		less_than_X++;
-	else
-		greater_than_X++;
-
-	std::cin >> n5;
-	if (n5 <= x)
-		less_than_X++;
-	else
-		greater_than_X++;
-
-	std::cout << less_than_X << " " << greater_than_X << std::endl;
 
 	return 0;
 }
