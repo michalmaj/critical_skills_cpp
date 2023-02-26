76e9e10a0f142cd403c5d29e4eb8e0b8511b7ac0
Michał Maj <michalpmaj@gmail.com>
2023-02-26 20:44:22 +0100

    Sort three numbers

--- 04_selection/6b_sort_three_numbers/Source.cpp
+++ 04_selection/6b_sort_three_numbers/Source.cpp
@@ -2,33 +2,36 @@
 
 int main()
 {
-	int a, b, c;
-	int f, s, t;
+	int a, b, c, temp;
 	std::cout << "Enter three numbers: ";
-	std::cin >> a;
-	f = a;
-	std::cin >> b;
-	s = b;
-	if(b < f)
+	std::cin >> a >> b >> c;
+
+	if(b < a) // Swap them
 	{
-		f = b;
-		s = a;
-	}
-	std::cin >> c;
-	t = c;
-	if(c < f)
-	{
-		t = s;
-		s = f;
-		f = c;
-	}
-	else if(c < s)
-	{
-		t = s;
-		s = c;
+		temp = a;
+		a = b;
+		b = temp;
 	}
 
-	std::cout << f << " " << s << " " << t << std::endl;
+	// Now a and b are correct
+	if(c < b) // Swap them
+	{
+		temp = b;
+		b = c;
+		c = temp;
+
+		// Now b, c are correct
+		// But a, b may not be again
+		if(b < a)
+		{
+			temp = a;
+			a = b;
+			b = temp;
+		}
+	}
+
+	std::cout << a << " " << b << " " << c << std::endl;
+
 
 
 
