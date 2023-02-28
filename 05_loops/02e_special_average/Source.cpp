720251df93c727307c236ff78bc0c307f234f04a
Michał Maj <michalpmaj@gmail.com>
2023-02-28 21:26:19 +0100

    Special average

---
+++ 05_loops/02e_special_average/Source.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main()
+{
+	int n;
+	int odd_n{ 0 }, odd_sum{ 0 };
+	int even_n{ 0 }, even_sum{ 0 };
+
+	std::cout << "Enter how many numbers do you want to read: ";
+	std::cin >> n;
+
+	int cnt{ 0 };
+	while(cnt < n)
+	{
+		++cnt;
+		int num;
+		std::cin >> num;
+
+		if(cnt % 2 == 0)
+		{
+			even_n++;
+			even_sum += num;
+			continue;
+		}
+
+		odd_n++;
+		odd_sum += num;
+	}
+
+	std::cout << odd_sum / odd_n << std::endl;
+	std::cout << even_sum / even_n << std::endl;
+
+
+	return 0;
+}
