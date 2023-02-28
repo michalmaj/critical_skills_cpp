5d00ab0c4e2b053994c31d8340fe1af1e2bf8695
Michał Maj <michalpmaj@gmail.com>
2023-02-28 21:31:47 +0100

    Print left angled triangle

--- 05_loops/02c_print_left_angled_triangle/Source.cpp
+++ 05_loops/02c_print_left_angled_triangle/Source.cpp
@@ -7,17 +7,17 @@ int main()
 	std::cout << "Enter how many rows do you want: ";
 	std::cin >> n;
 
-	int i{ 1 };
-	while(n--)
+	int row{ 1 };
+	while(row <= n)
 	{
-		int temp = i;
-		while(i--)
+		int stars_count = 1;
+		while(stars_count <= row)
 		{
 			std::cout << '*';
+			++stars_count;
 		}
 		std::cout << std::endl;
-		i = temp;
-		++i;
+		++row;
 	}
 	
 
