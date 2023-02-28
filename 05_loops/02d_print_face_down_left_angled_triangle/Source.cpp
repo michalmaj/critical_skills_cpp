734be8a99936b35d9c54e2e741613eb29b0f23c0
Michał Maj <michalpmaj@gmail.com>
2023-02-28 21:25:56 +0100

    Print face down left angled triangle

---
+++ 05_loops/02d_print_face_down_left_angled_triangle/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+	int n;
+
+	std::cout << "Enter how many rows do you want: ";
+	std::cin >> n;
+
+	int i{ n };
+	while (n--)
+	{
+		int temp = i;
+		while (i--)
+		{
+			std::cout << '*';
+		}
+		std::cout << std::endl;
+		i = temp;
+		--i;
+	}
+
+
+	return 0;
+}
