e28edb0de55d27ab1762b8af489143f77b9cdae9
Michał Maj <michalpmaj@gmail.com>
2023-03-01 21:29:36 +0100

    Special multiples 2

--- 05_loops/03c_special_multiples/Source.cpp
+++ 05_loops/03c_special_multiples/Source.cpp
@@ -7,15 +7,28 @@ int main()
 	std::cout << "Enter how many number do you want to find that is divisible by 3 but not by 4: ";
 	std::cin >> n;
 
-	int num{ -1 };
-	while (n)
+	//// First attempt: 
+	//int num{ -1 };
+	//while (n)
+	//{
+	//	++num;
+	//	if(num % 3 == 0 and num % 4 != 0)
+	//	{
+	//		std::cout << num << " ";
+	//		n--;
+	//	}
+	//}
+
+	// Second attempt
+	int start{ 3 };
+	while(n)
 	{
-		++num;
-		if(num % 3 == 0 and num % 4 != 0)
+		if(start % 4 != 0)
 		{
-			std::cout << num << " ";
-			n--;
+			--n;
+			std::cout << start << " ";
 		}
+		start += 3;
 	}
 
 	std::cout << std::endl;
