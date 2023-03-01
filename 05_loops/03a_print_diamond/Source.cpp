46194336fe15200c5ec94312672996ee96e4f333
Michał Maj <michalpmaj@gmail.com>
2023-03-01 21:25:05 +0100

    Print diamond

--- 05_loops/03a_print_diamond/Source.cpp
+++ 05_loops/03a_print_diamond/Source.cpp
@@ -7,49 +7,71 @@ int main()
 	std::cout << "Enter how many rows do you want: ";
 	std::cin >> n;
 
-	int n_copy = n;
-	int cnt{ 1 };
-	while(n)
+	/*
+	 * Let's print the upper triangle first.
+	 * Let's assume N = 4, how many spaces and starts we print.
+	 * Row 1		Spaces 3		Stars 1
+	 * Row 2		Spaces 2		Stars 3
+	 * Row 3		Spaces 1		Stars 5
+	 * Row 4		Spaces 0		Stars 7
+	 *
+	 * Now we want to develop formulas for number of spaces and number of stars.
+	 * For given 'row'
+	 *	Spaces are: N - row			(3, 2, 1, 0)
+	 *	Stars are: 2 * row - 1		(1, 3, 5, 7)
+	 *
+	 * Now we just iterate for each row
+	 *	print spaces
+	 *	then print stars
+	 */
+
+	int row = 1;
+	while(row <= n)
 	{
-		int tmp = n - 1;
-		int cnt2 = cnt;
-		while(tmp--)
+		int stars_count = 1;
+
+		// Print N - rows spaces
+		while(stars_count <= n - row)
 		{
 			std::cout << " ";
+			++stars_count;
 		}
 
-		while(cnt--)
+		// Print 2 * rows - 1 stars
+		stars_count = 1;
+		while (stars_count <= 2 * row - 1)
 		{
 			std::cout << "*";
+			++stars_count;
 		}
-
-		cnt2 +=2 ;
-		cnt = cnt2;
-		--n;
 		std::cout << std::endl;
+		++row;
 	}
 
-	n = n_copy;
-	cnt = 2 * n - 1;
-	while (n)
+	/*
+	 * Let's print the lower triangle.
+	 * Same logic, but we just switch looping from N to 1.
+	 */
+	row = n;
+	while(row >= 1)
 	{
-		int tmp = n_copy - n;
-		int cnt2 = cnt;
-		while (tmp--)
+		int stars_count{ 1 };
+
+		while(stars_count <= n - row)
 		{
 			std::cout << " ";
+			++stars_count;
 		}
 
-		while(cnt--)
+		stars_count = 1;
+		while(stars_count <= 2 * row - 1)
 		{
 			std::cout << "*";
+			++stars_count;
 		}
 
-		cnt2 -= 2;
-		cnt = cnt2;
-		--n;
 		std::cout << std::endl;
-
+		--row;
 	}
 
 
