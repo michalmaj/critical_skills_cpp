7154594e8eab5c39ac99fbf443a337d3f1a8f3aa
Michał Maj <michalpmaj@gmail.com>
2023-03-05 20:55:43 +0100

    Is prime

---
+++ 05_loops/06d_is_prime/Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main()
+{
+	int n;
+	std::cout << "Enter number to check if it is prime: ";
+	std::cin >> n;
+
+	if (n <= 1)
+	{
+		std::cout << "NO" << std::endl;
+		return 0;
+	}
+
+	if(n == 2)
+	{
+		std::cout << "YES" << std::endl;
+		return 0;
+	}
+
+	bool is_prime{ true };
+	for(int i{2}; i < n / 2 + 1; ++i)
+	{
+		if(n == i)
+			continue;
+
+		if (n % i == 0)
+			is_prime = false;
+	}
+
+	std::cout << ((is_prime) ? "YES" : "NO") << std::endl;
+
+	return 0;
+}
