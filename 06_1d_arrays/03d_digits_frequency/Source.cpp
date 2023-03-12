c8691131f424e30ed173762b1566b65586fe8b20
Michał Maj <michalpmaj@gmail.com>
2023-03-12 20:19:04 +0100

    Digits frequency

---
+++ 06_1d_arrays/03d_digits_frequency/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+	int frequency[10]{ 0 };
+	int N;
+	std::cout << "Enter how many numbers do you want to enter: ";
+	std::cin >> N;
+	
+	while(N--)
+	{
+		int num;
+		std::cin >> num;
+		while(num != 0)
+		{
+			frequency[num % 10]++;
+			num /= 10;
+		}
+	}
+
+	for (int i{ 0 }; i < std::size(frequency); ++i)
+		std::cout << i << " " << frequency[i] << std::endl;
+
+	return 0;
+}
