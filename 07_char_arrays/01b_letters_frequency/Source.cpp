e0bb85a04d7509594adb3865c54facffc08a1f8b
Michał Maj <michalpmaj@gmail.com>
2023-03-20 21:02:28 +0100

    Letters frequency

---
+++ 07_char_arrays/01b_letters_frequency/Source.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include <string>
+
+int main()
+{
+	std::string str{"bAAAaaazz"};
+	int frequency[26]{ 0 };
+	/*std::cout << "Enter a string: ";
+	std::cin >> str;*/
+
+	for(int i{0}; i < str.size(); ++i)
+	{
+		if (str[i] >= 'a' or 'z' <= str[i])
+		{
+			frequency[str[i] - 'a']++;
+		}
+	}
+
+	for(int i{0}; i < std::size(frequency); ++i)
+	{
+		if (frequency[i] != 0)
+			std::cout << static_cast<char>(i + 'a') << " " << frequency[i] << std::endl;
+	}
+
+
+	std::cout << std::endl;
+
+	return 0;
+}
