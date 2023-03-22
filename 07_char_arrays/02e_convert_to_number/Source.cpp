ebb171ff711b48e3ac17ab2a9a65cda49fe72e55
Michał Maj <michalpmaj@gmail.com>
2023-03-22 20:17:38 +0100

    Convert to number

--- 07_char_arrays/02e_convert_to_number/Source.cpp
+++ 07_char_arrays/02e_convert_to_number/Source.cpp
@@ -3,9 +3,12 @@
 
 int main()
 {
-	std::string str{ "0200" };
+	std::string str{ "12345" };
 	int num{ 0 };
 
+	// How to convert digits 1 2 3 4 5 to 12345
+	// Repeat: multiply with 10 then add digit
+	// 1 => 10 + 2 = 12 => 12 * 10 + 3 = 123 => 123 * 10 + 4 = 1234 => 1234 * 10 + 5 = 12345 
 	for(int i{0}; i < str.size(); ++i)
 	{
 		num *= 10;
