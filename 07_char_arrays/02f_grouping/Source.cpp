cd6bbc87a9c5e8e0e1b63e483911f41ec02ba19a
Michał Maj <michalpmaj@gmail.com>
2023-03-22 20:19:38 +0100

    Grouping

--- 07_char_arrays/02f_grouping/Source.cpp
+++ 07_char_arrays/02f_grouping/Source.cpp
@@ -4,17 +4,13 @@
 int main()
 {
 	std::string str{ "111222aabbb" };
-	char actual{ str.at(0) };
+
 
 	for(int i{0}; i < str.size(); ++i)
 	{
-		if (str.at(i) == actual)
-			std::cout << str.at(i);
-		else
-		{
-			std::cout << " " << str.at(i);
-			actual = str.at(i);
-		}
+		if (i != 0 and str[i - 1] != str[i]) // new group, split by space
+			std::cout << " ";
+		std::cout << str[i];
 	}
 
 
