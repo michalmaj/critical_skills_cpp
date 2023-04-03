68317a5d87454a0bf3f37cdd95d4b704d067b476
Michał Maj <michalpmaj@gmail.com>
2023-04-03 20:45:14 +0200

    Get nth prime

---
+++ 09_functions/02f_get_nth_prime/Source.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+bool is_prime(int num)
+{
+	if (num == 2 or num == 3)
+		return true;
+
+	for(int i{2}; i <= num / 2; ++i)
+	{
+		if (num % i == 0)
+			return false;
+	}
+
+	return true;
+}
+
+int nth_prime(int n)
+{
+	int it{ 1 };
+	while(n)
+	{
+		it++;
+
+		if (is_prime(it))
+			n--;
+	}
+	return it;
+}
+
+
+int main()
+{
+	std::cout << nth_prime(6) << std::endl;
+
+	for (int i{ 2 }; i < 19; ++i)
+		std::cout << std::boolalpha << i << ": " << is_prime(i) << std::endl;
+
+	return 0;
+}
