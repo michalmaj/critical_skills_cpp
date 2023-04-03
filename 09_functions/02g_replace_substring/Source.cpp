579cdd54040013c6cda64608faeffccdb5654766
Michał Maj <michalpmaj@gmail.com>
2023-04-03 21:14:30 +0200

    Replace substring

---
+++ 09_functions/02g_replace_substring/Source.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+bool starts_with(const std::string& input, const std::string& pattern, int pos)
+{
+	for(size_t i{0}; i < pattern.size(); ++i)
+	{
+		if (input.at(pos + i) != pattern.at(i))
+			return false;
+	}
+	return true;
+}
+
+std::string replace_str(const std::string& input, const std::string& pattern, const std::string& to)
+{
+	std::string output;
+
+	size_t it{ 0 };
+	while (it < input.size())
+	{
+		if (starts_with(input, pattern, it))
+		{
+			output += to;
+			it += pattern.size();
+		}
+		else
+		{
+			output += input.at(it);
+			it++;
+		}
+	}
+
+	return output;
+}
+
+int main()
+{
+	std::cout << replace_str("aabcabaaad", "aa", "") << std::endl;
+
+	return 0;
+}
