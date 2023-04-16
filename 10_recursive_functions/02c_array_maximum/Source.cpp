a9ec907fce3354a8daac11350f122ff3abdc9b0a
Michał Maj <michalpmaj@gmail.com>
2023-04-16 20:46:55 +0200

    Array maximum

--- 10_recursive_functions/02c_array_maximum/Source.cpp
+++ 10_recursive_functions/02c_array_maximum/Source.cpp
@@ -2,10 +2,12 @@
 
 int arr_max(int* arr, int n)
 {
+	// Base case
 	if (n == 1)
 		return arr[0];
 
-	return std::max(arr[n-1], arr_max(arr, n-1));
+	int sub_result = arr_max(arr, n - 1);
+	return std::max(arr[n-1], sub_result);
 
 }
 
