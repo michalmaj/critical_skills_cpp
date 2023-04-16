f7440b4bbf6b94576648e27e0e5220dd9b5967be
Michał Maj <michalpmaj@gmail.com>
2023-04-16 20:31:33 +0200

    Length of 3n+1 sequence

--- 10_recursive_functions/02a_length_of_3n_plus_1_sequence/Source.cpp
+++ 10_recursive_functions/02a_length_of_3n_plus_1_sequence/Source.cpp
@@ -2,29 +2,21 @@
 
 int length_3n_plus_1(int n)
 {
-	static int len{ 0 };
-
+	// Base case
 	if (n == 1)
-	{
-		len++;
-		return len;
-	}
+		return 1;
 
+	// If even, for example 6 3 10 5 16 8 4 2 1
 	if (n % 2 == 0)
-	{
-		len++;
-		length_3n_plus_1(n / 2);
-	}
-	else
-	{
-		len++;
-		length_3n_plus_1(3 * n + 1);
-	}
+		return 1 + length_3n_plus_1(n / 2);
+
+	// If odd
+	return 1 + length_3n_plus_1(3 * n + 1);
 }
 
 int main()
 {
-	std::cout << length_3n_plus_1(7) << std::endl;
+	std::cout << length_3n_plus_1(5) << std::endl;
 
 
 	return 0;
