181a2d04294f42ba1770572b98429da2cf4379f7
Michał Maj <michalpmaj@gmail.com>
2023-04-18 20:57:13 +0200

    Trace purpose

--- 10_recursive_functions/03f_trace_purpose/Source.cpp
+++ 10_recursive_functions/03f_trace_purpose/Source.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
 
-void do_something(int n)
+void do_something1(int n)
 {
 	if(n)
 	{
 		std::cout << n % 10;
-		do_something(n / 10);
+		do_something1(n / 10);
+	}
+}
+
+void do_something2(int n)
+{
+	if(n)
+	{
+		do_something2(n / 10);
+		std::cout << n % 10;
 	}
 }
 
 int main()
 {
-	do_something(123456);
+	do_something1(123456);
+	std::cout << std::endl;
+	do_something2(123456);
 
 	return 0;
 }
